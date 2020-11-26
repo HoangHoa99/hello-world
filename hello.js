15e799770036c40b6d4890678a61eded8de250e0
hoanghoa <40349029+HoangHoa99@users.noreply.github.com>
2020-11-26 09:01:59 +0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,10 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-})
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
